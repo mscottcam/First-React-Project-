0c02684c2c4dcb4ed42a549f8af46d4de8d906e1
mscottcam <mscottcam@gmail.com>
2017-07-26 15:06:11 -0500

    test

--- src/components/searchBox.js
+++ src/components/searchBox.js
@@ -1,11 +1,31 @@
 import React from 'react';
 
-export default function SearchBox(props) {
-    return (
-        <form name="calc" onSubmit={e => props.onSubmit(e)}>
-            <label htmlFor="input">Item</label>
-             <input onChange={e => props.onChange(e)} id="input" name="input" /> 
-            <button type="submit">Add</button>
-        </form>
-    )
+// export default function SearchBox(props) {
+//     return (
+//         <form name="calc" onSubmit={e => props.onSubmit(e)}>
+//             <label htmlFor="input">Item</label>
+//              <input onChange={e => props.onChange(e)} id="input" name="input" /> 
+//             <button type="submit">Add</button>
+//         </form>
+//     )
+// }
+
+export default class Form extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            input
+        }
+    }
+
+    render() {
+        return (
+            <form name="calc" onSubmit={e => props.onSubmit(e)}>
+                <label htmlFor="input">Item</label>
+                <input ref={input => this.state.input = input} onChange={e => props.onChange(e)} id="input" name="input" />
+                <button type="submit">Add</button>
+            </form>
+        )
+    }
+    
 }
